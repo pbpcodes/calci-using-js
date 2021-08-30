5e3a3f5fc37a5d781636b2e19e02f640c6fdc1cf
pbpcodes <pbpcodes.gmail.com>
2021-08-30 13:37:42 +0530

    added js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const display1E = document.querySelector('.display-1');
-const display2E = document.querySelector('.display-2');
-const currentResE = document.querySelector('.current-res');
+const display1Elem = document.querySelector('.display-1');
+const display2Elem = document.querySelector('.display-2');
+const currentResElem = document.querySelector('.current-res');
+const numbersElem = document.querySelector('.num');
+const operationsElem = document.querySelector('.ops');
+const equalsTo = document.querySelector('.equal');
+const clearScreen = document.querySelector('.clrscr');
+const clear = document.querySelector('.clr');
+
