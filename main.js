c50d49d6fff1c89cfc24b23024e0c2a4fb1e8daa
pbpcodes <pbpcodes.gmail.com>
2021-08-31 18:47:52 +0530

    added all functionality

--- main.js
+++ main.js
@@ -24,13 +24,13 @@ numbersElem.forEach(num => {
         display2Number += e.target.innerText;
         display2Elem.innerText = display2Number;
     })
-})
+});
 
 
 operationsElem.forEach(ops => {
     ops.addEventListener('click', (e)=>{
         if(!display2Number)
-        res;
+        return;
         dottt=false;
         const opName = e.target.innerText;
         if(display1Number && display2Number && lastOperation){
@@ -42,7 +42,7 @@ operationsElem.forEach(ops => {
         clearVar(opName);
         lastOperation=opName;
     })
-})
+});
 
 function clearVar(namee = ''){
     display1Number+=display2Number + ' ' + namee + ' ';
@@ -69,3 +69,90 @@ function mathOperation(){
         res = parseFloat(res)%parseFloat(display2Number);
     }
 }
+
+
+equalsTo.addEventListener('click', (e)=>{
+    if(!display1Number || !display2Number) 
+    return;
+    dottt=false;
+    mathOperation();
+    clearVar();
+    display2Elem.innerText = res;
+    currentResElem.innerText = '';
+    display2Number = res;
+    display1Number = '';
+    display1Elem.innerText='0';
+});
+
+clearScreen.addEventListener('click', (e)=>{
+    display2Elem.innerText='0';
+    display1Elem.innerText = '0';
+    display2Number = '';
+    display1Number = '';
+    res='';
+    currentResElem.innerText='0';
+});
+
+clear.addEventListener('click', (e)=>{
+    display2Elem.innerText='0';
+    display2Number = '';
+});
+
+//Takes input from keyboard too :)
+
+//Numbers
+window.addEventListener('keydown', (e)=>{
+    if(
+        e.key==='0' ||
+        e.key==='1' ||
+        e.key==='2' ||
+        e.key==='3' ||
+        e.key==='4' ||
+        e.key==='5' ||
+        e.key==='6' ||
+        e.key==='7' ||
+        e.key==='8' ||
+        e.key==='9' ||
+        e.key==='.'
+    ){
+        keyboardClick(e.key);
+    }
+});
+
+function keyboardClick(key){
+    numbersElem.forEach(button =>{
+        if(button.innerText===key){
+            button.click();
+        }
+    })
+}
+
+//Operations and equals
+
+window.addEventListener('keydown', (e)=>{
+    if(
+        e.key==='+' ||
+        e.key==='-' ||
+        e.key==='/' ||
+        e.key==='%' 
+    ){
+        keyboardClickOperations(e.key);
+    }
+    else if(e.key==='*'){
+        keyboardClickOperations('x');
+    }
+    else if(e.key==='=' || e.key=='Enter'){
+        equalsTo.click();
+
+    }
+});
+
+function keyboardClickOperations(key){
+    operationsElem.forEach(button=>{
+        if(button.innerText===key){
+            button.click();
+        }
+    })
+}
+
+
